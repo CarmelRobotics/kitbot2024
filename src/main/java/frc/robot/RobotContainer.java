b4e3f3c695e58510451d755be0a54db5cbb554ca
MrShoe_ <mrshoeshoe66@gmail.com>
2024-01-23 13:06:33 -0800

    Init & Shooter

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.Drive;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.ShooterShoot;
+import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.Shooter;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DriveTrain m_drivetrain = new DriveTrain();
+  private final Joystick j_joystick = new Joystick(Constants.Controls.CONTROLS_USB_JOYSTICK);
+  private final JoystickButton shoot;
+  private final JoystickButton eat;
+  // private final JoystickButton b_cylinder_up;
+  // private final JoystickButton b_cylinder_dn;
+  private final Shooter schoolshooter =  new Shooter(); 
+  
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+   // b_cylinder_up = new JoystickButton(j_joystick, Constants.Controls.CONTROLS_BUTTON_CYLINDER_UP);
+    //b_cylinder_dn = new JoystickButton(j_joystick, Constants.Controls.CONTROLS_BUTTON_CYLINDER_DN);
+    shoot = new JoystickButton(j_joystick, 3);
+    eat = new JoystickButton(j_joystick,4);
+    
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    m_drivetrain.setDefaultCommand(new Drive(m_drivetrain, j_joystick));
+    shoot.whileHeld(new ShooterShoot(1));
+    eat.whileHeld(new ShooterShoot(-1));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new ExampleCommand(null);
+  }
+}
---
+++ src/main/java/frc/robot/commands/ShooterShoot.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterShoot extends CommandBase {
+    private Shooter shooter;
+    private double speed;
+
+    public ShooterShoot(double speed) {
+        shooter = new Shooter();
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize(){}
+    @Override
+    public void execute(){
+        shooter.set(speed);
+    }
+    @Override
+    public void end(boolean interrupted){
+        shooter.set(0);   
+    }
+    @Override
+    public boolean isFinished(){return false;}
+}
